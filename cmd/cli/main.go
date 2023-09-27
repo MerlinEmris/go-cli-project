ab4db74ec0da095e1e1025c2d2052d36690e8431
Merdan Chariyarov <merdanchariyarov@gmail.com>
2023-09-27 14:09:39 +0400

    feat: project structure end

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/MerlinEmris/go-cli-project/internal/network"
+)
 
 func main() {
 	fmt.Println("Entrypoint")
+	network.Ping("127.0.0.1")
 }
---
+++ internal/network/ping.go
@@ -0,0 +1,9 @@
+package network
+
+import "fmt"
+
+//Ping - take in an IP and ping that ip before
+// return a response
+func Ping(ip string) {
+	fmt.Println(ip)
+}
